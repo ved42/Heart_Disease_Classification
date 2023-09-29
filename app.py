bec6c350f00e097b59fdfc41c32840f05843b54c
ved42 <kokanevedant7@gmail.com>
2023-09-29 08:27:52 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
-from flask import Flask
+from flask import Flask, request, app,render_template
+from flask import Response
+import pickle
+import numpy as np
+import pandas as pd
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+
+scaler=pickle.load(open("/config/workspace/model/standardScaler.pkl", "rb"))
+model = pickle.load(open("/config/workspace/model/ModelForPrediction.pkl", "rb"))
+
+## Route for homepage
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+## Route for Single data point prediction
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    result=""
+
+    if request.method=='POST':
+
+        age=int(request.form.get("age"))
+        sex = int(request.form.get('sex'))
+        cp = int(request.form.get('cp'))
+        trestbps = int(request.form.get('trestbps'))
+        chol = int(request.form.get('chol'))
+        fbs = int(request.form.get('fbs'))
+        restecg = int(request.form.get('restecg'))
+        thalach = int(request.form.get('thalach'))
+        exang = int(request.form.get('exang'))
+        oldpeak = float(request.form.get('oldpeak'))
+        slope = int(request.form.get('slope'))
+        ca = int(request.form.get('ca'))
+        thal = int(request.form.get('thal'))
+
+        new_data=scaler.transform([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        predict=model.predict(new_data)
+       
+        if predict[0] ==1 :
+            result = 'Facing Heart Disease'
+        else:
+            result ='Not Facing Heart Disease'
+            
+        return render_template('single_prediction.html',result=result)
+
+    else:
+        return render_template('home.html')
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
